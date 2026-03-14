70686282b5d6ff4f29f700fce9a6af0f96f633d1
Chung jaemin <hijam76@gmail.com>
2026-03-14 21:40:01 +0900

    122

--- logic.py
+++ logic.py
@@ -1,17 +1,21 @@
-class Battelfild():
+class Battlefield():
     Battelfild:list[list[int]]
 
-    def attack(pos:tuple):
-        c_block = Battelfild[pos[0]][pos[1]]
+    def __init__(self):
+        pass
+
+    def attack(self,pos:tuple):
+        c_block = Battlefield[pos[0]][pos[1]]
         if c_block == 2 or c_block ==2:
             return False
         elif c_block == 0:
             c_block = 2
         elif c_block == 1:
             c_block = 3
-    def get():
+
+    def get(self):
         tmp = []
-        for i in Battelfild:
+        for i in Battlefield:
             for j in i:
                 tmp.append(j)
         return tmp
