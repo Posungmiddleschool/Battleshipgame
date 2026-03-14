5f3ab6d6ae81dcd65b92afaf096949510a3bd7ca
Chung jaemin <hijam76@gmail.com>
2026-03-14 21:27:52 +0900

    123

--- logic.py
+++ logic.py
@@ -1,5 +1,17 @@
 class Battelfild():
     Battelfild:list[list[int]]
-    def attack(pos:tuple){
-        
-    }
+
+    def attack(pos:tuple):
+        c_block = Battelfild[pos[0]][pos[1]]
+        if c_block == 2 or c_block ==2:
+            return False
+        elif c_block == 0:
+            c_block = 2
+        elif c_block == 1:
+            c_block = 3
+    def get():
+        tmp = []
+        for i in Battelfild:
+            for j in i:
+                tmp.append(j)
+        return tmp
